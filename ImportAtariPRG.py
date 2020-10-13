3db4e6b1d41c42933e6937cbfe39e389812fd3de
Thorsten Otto <admin@tho-otto.de>
2020-10-13 19:02:56 +0200

    Add support for GNU symbol tables

--- ImportAtariPRG.py
+++ ImportAtariPRG.py
@@ -93,7 +93,22 @@ if rea != 0:
         rea = rea + offs
         # print("Relocating %x (%08x => %08x)" % (rea, mem.getInt(prg.add(rea)), mem.getInt(prg.add(rea))+reloc_addr))
         mem.setInt(prg.add(rea), mem.getInt(prg.add(rea)) + reloc_addr)
+        # when we are already in the data segment, create a dword here
+        if (rea >= len_text):
+            data = flat.createDWord(prg.add(rea)) # FIXME: should be pointer
 
+sym_format = -1
+#
+# check for GNU binutils aexec header
+#
+if len_text > 0xe4 and mem.getInt(start.add(18)) == 0x4d694e54 and ((mem.getInt(start.add(28)) == 0x283a001a and mem.getInt(start.add(32)) == 0x4efb48fa) or (mem.getInt(start.add(28)) == 0x203a001a and mem.getInt(start.add(32)) == 0x4efb08fa)) and (mem.getShort(start.add(38)) == 0x0108 or mem.getShort(start.add(38)) == 0x010b):
+    a_syms = mem.getInt(start.add(52))
+    sym_format = mem.getInt(start.add(80))
+elif len_sym > 0:
+    sym_format = 1
+# Primitive plausibility check: a DRI/GST symbol table size always is a multiple of 14
+if len_sym == 0 or (sym_format == 1 and (len_sym % struct.calcsize(">8sHL")) != 0):
+    sym_format = -1
 
 # Header Block, split off actual TEXT, DATA sections
 bl_hdr = mem.getBlocks()[0] # only one block exists right now
@@ -130,8 +145,27 @@ frag = program.getTreeManager().getFragment(ghidra.program.database.module.TreeM
 frag.setName("Header, TEXT, DATA")
 
 # Import symbol table if the user wants to
-# Primitive plausibility check: a DRI/GST symbol table size always is a multiple of 14
-if len_sym > 0 and (len_sym % struct.calcsize(">8sHL")) == 0 and askYesNo("Import", "Import symbol table?") == True:
+if sym_format >= 0:
+    if askYesNo("Import", "Import symbol table?") != True:
+        sym_format = -1
+
+# because Ghidra's Python doesn't have this
+def my_iter_unpack(fmt, buffer):
+    fmtsize = struct.calcsize(fmt)
+    for i in range(0, len(buffer), fmtsize):
+        yield struct.unpack_from(fmt, buffer, i)
+
+# get a zero terminated string from a buffer, starting at pos
+def get_string(s, pos):
+   term = s.find(b"\0", pos + 1)
+   if term != -1:
+      return s[pos:term].decode("ascii")
+   else:
+      return s[pos:].decode("ascii")
+
+# parse symbol table into list
+symbols = []
+if sym_format == 1:
     s_ptr = 0
     while s_ptr < len_sym:
         s_name, s_id, s_addr = struct.unpack_from(">8sHL", bytes(sym_table), s_ptr)
@@ -152,20 +186,80 @@ if len_sym > 0 and (len_sym % struct.calcsize(">8sHL")) == 0 and askYesNo("Impor
 
         s_type = s_id & 0xf00
         if s_type == 0x200: # TEXT
-            flat.createLabel(text_start.add(s_addr), s_name, False)
+            symbols += [{"name":s_name,"addr":text_start.add(s_addr)}]
         elif s_type == 0x400: # DATA
-            flat.createLabel(data_start.add(s_addr), s_name, False)
+            symbols += [{"name":s_name,"addr":data_start.add(s_addr)}]
         elif s_type == 0x100: # BSS
-            flat.createLabel(bss_start.add(s_addr),  s_name, False)
+            symbols += [{"name":s_name,"addr":bss_start.add(s_addr)}]
         else: # unsupported type
             pass
+elif sym_format == 0:
+    string_size = struct.unpack_from(">l", bytes(sym_table[a_syms:a_syms+4]))[0]
+    #print("a_sym size: %x" % (a_syms))
+    #print("sym size: %x" % (len_sym))
+    #print("string size: %x %x" % (string_size, len_sym - a_syms))
+    string_table = bytes(sym_table[a_syms:a_syms+4+string_size])
+    # fictious section for external references
+    ext_size = len_bss
+    sym_table = bytes(sym_table[0:a_syms])
+    for (n_strx, n_type, n_other, n_desc, n_value) in my_iter_unpack(">lBbhL", sym_table):
+        if n_strx == 0:
+            continue
+        n_name = get_string(string_table, n_strx)
+        #print("s: %08x %s" % (n_strx, n_name))
+
+        # skip symbols containing file names
+        if n_name[-2:] == ".o" or n_name[-2:] == ".a" or n_name.find("/") >= 0:
+            continue
+
+        n_type &= 0x1f # mask for type
+        if n_type == 0x00 or n_type == 0x01: # undefined or common
+            if n_value == 0: # undefined=external symbol
+                # create external symbol arbitrarily sized 4 bytes
+                # n_addr = bss_start.add(ext_size)
+                n_addr = None
+                ext_size += 4
+            else: # common symbol = uninitialized variable with given length
+                # add to bss
+                # n_addr = bss_start.add(ext_size)
+                n_addr = None
+                ext_size += n_value
+        elif n_type == 0x02 or n_type == 0x03 or n_type == 0x0e: # absolute
+            # n_addr = n_value # TODO: create absolute symbol
+            n_addr = None
+        elif n_type == 0x04 or n_type == 0x05 or n_type == 0x0f: # text
+            n_addr = text_start.add(n_value)
+            #print("%08x T %s" % (n_addr.offset, n_name))
+        elif n_type == 0x06 or n_type == 0x07 or n_type == 0x10: # data
+            n_addr = data_start.add(n_value - len_text)
+            #print("%08x D %s" % (n_addr.offset, n_name))
+        elif n_type == 0x08 or n_type == 0x09 or n_type == 0x11: # bss
+            n_addr = bss_start.add(n_value - len_text - len_data)
+            #print("%08x B %s" % (n_addr.offset, n_name))
+        elif n_type == 0x1c or n_type == 0x1d:                   # N_SETV; ignored
+            n_addr = None
+        else:
+            print("Unknown symbol type %d" % n_type)
+            n_addr = None
+        if n_addr != None:
+            symbols += [{"name":n_name,"addr":n_addr}]
+
+# add symbols
+for sym in symbols:
+    # skip local labels
+    if sym["name"][0:2] != ".L":
+        flat.createLabel(sym["addr"], sym["name"], False)
 
 # Add some labels
-flat.createDwords(start.add(0x2), 4)
+flat.createDwords(start.add(0x2), 6)
 flat.createLabel(start.add(0x2), "TEXT_LEN", True)
 flat.createLabel(start.add(0x6), "DATA_LEN", True)
 flat.createLabel(start.add(0xa), "BSS_LEN", True)
 flat.createLabel(start.add(0xe), "SYM_LEN", True)
+flat.createLabel(start.add(0x12), "RESERVED", True)
+flat.createLabel(start.add(0x16), "PRGFLAGS", True)
+flat.createWord(start.add(0x1A))
+flat.createLabel(start.add(0x1A), "ABSOLUTE", True)
 flat.createLabel(text_start, "ENTRY_POINT", True)
 
 # Run disassembler on entry point
@@ -174,4 +268,3 @@ flat.disassemble(text_start)
 # Open for user to see and to start analyzing
 program.endTransaction(txn, True)
 openProgram(program)
-
